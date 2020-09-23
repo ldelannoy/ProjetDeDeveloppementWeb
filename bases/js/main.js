f7fc997c468246194ed028513a09dee65d5b250a
ldelannoy <lrt.delannoy@gmail.com>
2020-09-23 12:12:47 +0200

    tableau bootsrap afficher/cacher

--- bases/js/main.js
+++ bases/js/main.js
@@ -1,2 +1,23 @@
-console.log('Mon message');
-console.error('Mon message d\'erreur');
+//console.log('Mon message');
+//console.error('Mon message d\'erreur');
+//document.addEventListener('click',function(){
+ //   alert ('Message');
+//});
+
+const myButton = document.getElementById('bouton-bleu');
+//console.log('myButton');
+if(myButton != null){
+myButton.addEventListener('click',function(){
+    //alert ('test');});
+    const table = document.querySelector('table');
+    if(getComputedStyle(table).display != 'none'){
+    table.style.display ='none';
+    }else{
+        table.style.display = 'table';
+    }
+});
+}
+
+//const btns =document.getElementsByClassName('btn');
+
+//btns[0].addEventListener('click',function(){alert('Message');});
